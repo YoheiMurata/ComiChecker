8c0b2d9cd5b0273d79f98d6f78f7111206591137
YoheiMurata <fullmetaljacket893@gmail.com>
2015-12-27 22:13:15 +0900

    不要なSQLを削除

--- sql/LOG.sql
+++
@@ -1,4 +0,0 @@
-# Twitter RESt APIのアクセス回数を
-# API別に記録したい（願望）
-CREATE TABLE ACCESS_LOG(
-	
--- sql/TWEET.sql
+++ sql/TWEET.sql
@@ -10,15 +10,6 @@ CREATE TABLE TWEET (
 	TWEETED_BY BIGINT UNSIGNED NOT NULL,
 	# ツイートの内容
 	TEXT VARCHAR( 280 ) NOT NULL,
-	# 画像などが付いてたらバッチ側で魚拓として画像を保存する。
-	# その画像が保存されたローカルホストの位置を参照する
-	# ＊よく考えたらメディアは１つとは限らないので、
-	#   別のテーブルに保存する必要がある
-	# TWEET_MEDIA_PATH VARCHAR( 256 ) DEFAULT NULL,
-	# メディアのURL.HTTP版とHTTPS版があるがHTTPS版を
-	# 記録することにする
-	# 上の＊と同じ理由でこれも削除し、別のテーブルに保存する
-	# TWEET_MEDIA_URL_PATH VARCHAR( 512 ) DEFAULT NULL,
 	# 誰がツイートしたのかカラムの外部キー制約
 	FOREIGN KEY(TWEETED_BY)
 	REFERENCES TWITTER_USER(TWITTER_USER_ID)
